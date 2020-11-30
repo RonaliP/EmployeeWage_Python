50f2180d2a6233817e01bcf7498b57237aee39c1
Ronali <ronalipanigrahy88@gmail.com>
2020-11-30 11:52:29 +0530

    PART TIME WORKING HOURS ADDED

--- Employeewage.py
+++ Employeewage.py
@@ -1,10 +1,18 @@
 import random
+Total_workinghours=8
+_wageperhour=20
+Part_time_hours=4
+
 def checkattendance():
     if random.randint(0,1) == 1:
         print("Employee is present today")
     else:
         print("Employee is absent today")
 
+def calculatewage():
+    print("Dailywage of employee is", Total_workinghours*_wageperhour)
+
 if __name__ == '__main__':
     print("WELCOME TO EMPLOYEE WAGE CALCULATION PROGRAM")
     checkattendance()
+    calculatewage()
