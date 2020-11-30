388dc6bff85a9f22d953425fdc59228a9a16feda
Ronali <ronalipanigrahy88@gmail.com>
2020-11-30 11:26:38 +0530

    CODE FOR CHECKING ATTENDANCE ADDED

--- Employeewage.py
+++ Employeewage.py
@@ -1,1 +1,10 @@
-print("WELCOME TO EMPLOYEE WAGE CALCULATION PROGRAM")
+import random
+def checkattendance():
+    if random.randint(0,1) == 1:
+        print("Employee is present today")
+    else:
+        print("Employee is absent today")
+
+if __name__ == '__main__':
+    print("WELCOME TO EMPLOYEE WAGE CALCULATION PROGRAM")
+    checkattendance()
